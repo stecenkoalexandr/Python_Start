7c20bc11f6d7c8f2d58294812b8cd56170830aa8
Oleksandr Stetsenko <stecenkoalexandr@gmail.com>
2022-09-19 23:28:27 +0300

    Update main.py
    2

--- home_1/main.py
+++ home_1/main.py
@@ -1,1 +1,13 @@
 print("Hello World!")
+
+print("Here we wilbe study Python")
+
+print("I thought Python is very useful language")
+
+print("Many companies use Python for their projects")
+
+# Exercise 3. Write a Python-script to reads values for the length and width of a rectangle and returns the area of the rectangle
+
+a = int(input("Dear user please input lenght"))
+b = int(input("Dear user please input width"))
+print(f "Area of the rectangle will be "{a*b})
