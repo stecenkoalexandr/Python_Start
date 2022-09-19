d24c55affaf4477ac2ea13ba1282bb6beb8e4665
Oleksandr Stetsenko <stecenkoalexandr@gmail.com>
2022-09-20 00:07:30 +0300

    Update and rename main.py to ht_1.py
    3

---
+++ home_1/ht_1.py
@@ -0,0 +1,21 @@
+print("Hello World!")
+# ---------------------------------------------------------------
+print("Here we wilbe study Python")
+
+print("I thought Python is very useful language")
+
+print("Many companies use Python for their projects")
+print ("Igor", "Oleg", "Dima", sep='\n' end='\n')
+# Намагаємось все писати в одному рядку, для того щоб мінімалізувати ресурси
+# -----------------------------------------------------------------
+# Exercise 3. Write a Python-script to reads values for the length and width of a rectangle and returns the area of the rectangle
+
+a = int(input("Dear user please input lenght"))
+b = int(input("Dear user please input width"))
+print(f "Area of the rectangle will be "{a*b})
+
+# Exercise 4 Write a program that requests the user to enter two numbers and  prints the sum, product, difference and quotient of the two numbers
+
+c = int(input("Please input number#1"))
+d = int(input("Please input number#2"))
+print()
