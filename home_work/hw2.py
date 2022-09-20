038a2b13c1bb24fe3c86017bb57b2add9350de7f
Oleksandr Stetsenko <stecenkoalexandr@gmail.com>
2022-09-20 15:18:02 +0300

    1

--- home_work/hw2.py
+++ home_work/hw2.py
@@ -1,1 +1,5 @@
-print("Hello World!")
+# 1. Write a Python program to print the number entered by user only if the number entered is negative.
+# 2. Write a Python program to check if the value a is less than 20 or not.
+# 3. Write a Python program to check if a given number is Zero or Not.
+# 4. Write a Python program to check if a given number is Even or Odd.
+# 5. Write a Python program to find largest number among three numbers entered by user.
